53e04c41f5ae43fcaa3b8129f7b84384391723d3
cristian.lopera <cristian1020011@gmail.com>
2019-09-09 19:43:12 -0500

    db

--- villa_campestre.sql
+++ villa_campestre.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 07, 2019 at 09:37 PM
+-- Generation Time: Sep 10, 2019 at 02:41 AM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.2.20
 
@@ -25,6 +25,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `habitacion`
+--
+
+CREATE TABLE `habitacion` (
+  `hab_numero` int(11) NOT NULL,
+  `id_tipo_habitacion` int(11) NOT NULL,
+  `hab_detalle` longtext NOT NULL,
+  `hab_fecha_creacion` date NOT NULL,
+  `hab_estado` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `habitacion`
+--
+
+INSERT INTO `habitacion` (`hab_numero`, `id_tipo_habitacion`, `hab_detalle`, `hab_fecha_creacion`, `hab_estado`) VALUES
+(1, 1, 'nada', '0000-00-00', 0),
+(2, 1, '', '2019-09-08', 1),
+(3, 1, '', '2019-09-08', 1),
+(4, 1, '', '2019-09-08', 1),
+(5, 1, '', '2019-09-08', 1),
+(6, 1, '', '2019-09-08', 1),
+(7, 1, '', '2019-09-08', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `rol`
 --
 
@@ -41,6 +68,32 @@ CREATE TABLE `rol` (
 INSERT INTO `rol` (`rol_id`, `rol_nombre`, `rol_descripcion`) VALUES
 (1, 'empleado', 'caja'),
 (2, 'administrador', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipo_habitacion`
+--
+
+CREATE TABLE `tipo_habitacion` (
+  `id_tipo_habitacion` int(11) NOT NULL,
+  `th_nombre_tipo` varchar(50) NOT NULL,
+  `th_descripcion` longtext NOT NULL,
+  `th_valor_hora` bigint(20) NOT NULL,
+  `th_valor_persona_adicional` bigint(20) NOT NULL,
+  `th_estado` int(1) NOT NULL,
+  `th_fecha_creacion` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tipo_habitacion`
+--
+
+INSERT INTO `tipo_habitacion` (`id_tipo_habitacion`, `th_nombre_tipo`, `th_descripcion`, `th_valor_hora`, `th_valor_persona_adicional`, `th_estado`, `th_fecha_creacion`) VALUES
+(1, 'Premium', 'Cama doble para tiki tiki', 20000, 12000, 1, '2019-09-07'),
+(2, 'Premium2', 'des', 23232, 12000, 1, '2019-09-07'),
+(3, 'Premium3', 'des', 23232, 1, 1, '2019-09-07'),
+(4, 'Premium4', 'des', 23232, 1, 1, '2019-09-07');
 
 -- --------------------------------------------------------
 
@@ -75,10 +128,24 @@ INSERT INTO `usuario` (`usu_id`, `usu_nombres`, `usu_apellidos`, `usu_numero_doc
 --
 
 --
+-- Indexes for table `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD PRIMARY KEY (`hab_numero`),
+  ADD KEY `id_tipo_habitacion` (`id_tipo_habitacion`);
+
+--
 -- Indexes for table `rol`
 --
 ALTER TABLE `rol`
   ADD PRIMARY KEY (`rol_id`);
+
+--
+-- Indexes for table `tipo_habitacion`
+--
+ALTER TABLE `tipo_habitacion`
+  ADD PRIMARY KEY (`id_tipo_habitacion`),
+  ADD UNIQUE KEY `th_nombre_tipo` (`th_nombre_tipo`);
 
 --
 -- Indexes for table `usuario`
@@ -98,6 +165,12 @@ ALTER TABLE `rol`
   MODIFY `rol_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
+-- AUTO_INCREMENT for table `tipo_habitacion`
+--
+ALTER TABLE `tipo_habitacion`
+  MODIFY `id_tipo_habitacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `usuario`
 --
 ALTER TABLE `usuario`
@@ -106,6 +179,12 @@ ALTER TABLE `usuario`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `habitacion`
+--
+ALTER TABLE `habitacion`
+  ADD CONSTRAINT `habitacion_ibfk_1` FOREIGN KEY (`id_tipo_habitacion`) REFERENCES `tipo_habitacion` (`id_tipo_habitacion`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `usuario`
