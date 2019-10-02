340fa7a8a0e6c6965ee13ced07c0ed5a708e8e55
Evelin <ginatha97@gmail.com>
2019-10-01 23:03:41 -0500

    Ajustes en facturas

--- controller/reporte.controller.php
+++ controller/reporte.controller.php
@@ -44,6 +44,76 @@ Class ReporteController{
         }
         $result = array("status"=>$status,"message"=>$message,"data"=>$data);
         echo json_encode($result);
-    }    
+    }
+    
+    function reporteVentas(){
+        header('Content-Type:application/json');
+        if(!empty($_POST)){
+            $request = $_POST;
+            $catidadDeFacturas = $this->masterModel->sqlSelect("SELECT COUNT(*) as catidadDeFacturas FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"))[0]->catidadDeFacturas;
+            $totalEfectivo = number_format($this->masterModel->sqlSelect("SELECT SUM(fac_valor_efectivo) as totalEfectivo FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"))[0]->totalEfectivo);
+            $totalCredito = number_format($this->masterModel->sqlSelect("SELECT SUM(fac_valor_credito) as totalCredito FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"))[0]->totalCredito);
+            $totalTransferencia = number_format($this->masterModel->sqlSelect("SELECT SUM(fac_valor_transferencia) as totalTransferencia FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"))[0]->totalTransferencia);
+            $totalVentas = number_format($this->masterModel->sqlSelect("SELECT SUM(valor_factura) as totalTransferencia FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"))[0]->totalTransferencia);
+            $facturasRealizadas = $this->masterModel->sqlSelect("SELECT *  FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"));
+            $productos = array();
+            if(!empty($catidadDeFacturas)){
+                foreach($facturasRealizadas as $fatura){
+                    $fatura->productos = $this->masterModel->sqlSelect("SELECT df.*,p.pro_nombre FROM detalle_factura df INNER JOIN  producto p ON p.id_producto = df.det_id_producto WHERE  fac_consecutivo =  ?",array($fatura->fac_consecutivo));
+                    $productos[]=$this->masterModel->sqlSelect("SELECT df.*,p.pro_nombre FROM detalle_factura df INNER JOIN  producto p ON p.id_producto = df.det_id_producto WHERE  fac_consecutivo =  ?",array($fatura->fac_consecutivo));
+                }
+                $status = "success";
+                $message = "Consultas realizada.";
+                $data = array(
+                    "catidadDeFacturas"=>$catidadDeFacturas,
+                    "totalEfectivo"=>$totalEfectivo,
+                    "totalCredito"=>$totalCredito,
+                    "totalTransferencia"=>$totalTransferencia,
+                    "totalVentas"=>$totalVentas,
+                    "facturasRealizadas" => $facturasRealizadas,
+                    "productosVendidos" =>$productos
+                );
+            }else{
+                header('Internal server error', true, 500);
+                $status = "error";
+                $message = "no hay información asociada a esta consulta verifica los parametros.";
+                $data = null;
+            }
+            $result = array("status"=>$status,"message"=>$message,"data"=>$data);
+            echo json_encode($result);
+        }else{
+            header('405 Method Not Allowede', true, 405);
+        }
+    }
+    function reporteFormarDePago(){
+        header('Content-Type:application/json');
+        if(!empty($_POST)){
+            $request = $_POST;
+            $totalEfectivo = number_format($this->masterModel->sqlSelect("SELECT SUM(fac_valor_efectivo) as totalEfectivo FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"))[0]->totalEfectivo);
+            $totalCredito = number_format($this->masterModel->sqlSelect("SELECT SUM(fac_valor_credito) as totalCredito FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"))[0]->totalCredito);
+            $totalTransferencia = number_format($this->masterModel->sqlSelect("SELECT SUM(fac_valor_transferencia) as totalTransferencia FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"))[0]->totalTransferencia);
+            $totalVentas = number_format($this->masterModel->sqlSelect("SELECT SUM(valor_factura) as totalTransferencia FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"))[0]->totalTransferencia);
+            $facturasRealizadas = $this->masterModel->sqlSelect("SELECT *  FROM facturas WHERE fac_hora_salida BETWEEN ? AND ?",array($request["fecha_inicio"]." 00:00:01",$request["fecha_final"]." 23:59:59"));
+            if(!empty($facturasRealizadas)){
+                $status = "success";
+                $message = "Consultas realizada.";
+                $data = array(
+                    "totalEfectivo"=>$totalEfectivo,
+                    "totalCredito"=>$totalCredito,
+                    "totalTransferencia"=>$totalTransferencia,
+                    "totalVentas"=>$totalVentas
+                );
+            }else{
+                header('Internal server error', true, 500);
+                $status = "error";
+                $message = "no hay información asociada a esta consulta verifica los parametros.";
+                $data = null;
+            }
+            $result = array("status"=>$status,"message"=>$message,"data"=>$data);
+            echo json_encode($result);
+        }else{
+            header('405 Method Not Allowede', true, 405);
+        }
+    }
 }
 ?>
